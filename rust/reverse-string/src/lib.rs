b8dd97fd4dfe554e372fe8753e0ffbce2b0a21a6
Surya <suryamailsyou@gmail.com>
2021-02-22 14:41:36 +0530

    reversing string

---
+++ rust/reverse-string/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn reverse(input: &str) -> String {
+    let mut length = input.len();
+    let mut result = String::new();
+    if length == 0 {
+        return result;
+    }
+
+    let char_vec: Vec<char> = input.chars().collect();
+    length = char_vec.len();
+    for x in 0..length {
+        result.push(char_vec[length - 1 - x]);
+    }
+
+    result
+}
